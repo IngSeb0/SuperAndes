0a18acfded5f78651cca5028ddf56447a24373aa
Jssoff <js.garay@uniandes.com>
2024-11-03 00:02:36 -0500

    s

--- parranderos/src/main/java/uniandes/edu/servicios/BodegaServicio.java
+++ parranderos/src/main/java/uniandes/edu/servicios/BodegaServicio.java
@@ -18,17 +18,15 @@ public class BodegaServicio {
     @Autowired
     private BodegaRepository bodegaRepository;
 
-    // Método para consultar documentos de ingreso de productos a bodega.
+   
     @Transactional(isolation = Isolation.READ_COMMITTED, readOnly = true)
     public Collection<Map<String, Object>> obtenerDocumentosIngresoProductos(Long idSucursal, Long idBodega) throws InterruptedException {
-        // Consultar los documentos de ingreso de productos.
+        
         Collection<Map<String, Object>> documentos = bodegaRepository.obtenerDocumentosIngresoProductos(idSucursal, idBodega);
 
-        // Temporizador de 30 segundos para simular operación larga.
         Thread.sleep(30000);
 
-        // Consultar nuevamente los documentos, si es necesario.
         documentos = bodegaRepository.obtenerDocumentosIngresoProductos(idSucursal, idBodega);
-        return documentos; // Devolver los documentos consultados.
+        return documentos; 
     }
 }
